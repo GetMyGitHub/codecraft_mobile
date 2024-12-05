9153fc270bc4b98ef1645d392aa7714756626c40
Christophe Pauliac <c.pauliac@ag2l.fr>
2024-12-05 17:56:06 +0100

    run 2

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Home"),
+      child: const Text("Home"),
     );
   }
 }
